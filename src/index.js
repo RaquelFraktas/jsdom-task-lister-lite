3791921886b933add31ac6c4008532c9816e67ca
github username <github email address>
2021-08-04 21:02:19 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  submitOnClick();
+  
 });
+
+const taskForm = document.querySelector("#create-task-form");
+const newTaskDescription = document.getElementById("#new-task-description").value;
+const taskList = document.getElementById("#tasks");
+
+
+function submitOnClick(){
+  taskForm.addEventListener("click", function(e){
+    e.preventDefault();
+    taskList.innerHTML+= newTaskDescription;
+  });
+}
